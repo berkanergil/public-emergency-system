afd2068a395b8c187071e2c3e6cbda502f46f52f
berkan <berkannergil@gmail.com>
2022-01-05 21:45:08 +0200

    create done

--- resources/views/authority/form_agent_groups.blade.php
+++ resources/views/authority/form_agent_groups.blade.php
@@ -1,8 +1,10 @@
 @extends('authority.dashboard')
 @section('breadcrumb')
-    <a>Form Agent Groups</a>
+<a>Form Agent Groups</a>
 @endsection
 @section('statistic_content')
+<form action="{{ route("form_agent_groups") }}" method="post">
+    @csrf
     <div class="container-fluid bg-white rounded p-4">
 
         <div>
@@ -13,71 +15,28 @@
                 <div class="col-md-12">
                     <h6 class="text-danger text-bold">Select Agent</h6>
                     <div class="border p-5 ">
-                        <select class="form-select form-select-lg mb-3 available_agents"  multiple="multiple">
+                        <select class="form-select form-select-lg mb-3 available_agents " required name="agents_id[]"
+                            multiple="true">
                             @foreach ($staffObject->availableAgents() as $agent)
-                            <option value={{ $agent->id }}>{{ $agent->name." ".$agent->surname }}</option>
+                            <option value={{ $agent->id }}>{{ $agent->name." ".$agent->surname}}</option>
                             @endforeach
                         </select>
-                        <button id="add_staff" class="btn btn-md btn-info"><i class="fas fa-plus-square"></i> Add</button>
+                        <button class="btn btn-md btn-info"><i class="fas fa-plus-square"></i> Add</button>
                     </div>
-
                 </div>
             </div>
-            {{-- <div class="row mt-4 text-center">
-                <div class="container-fluid">
-                    <h5 class="text-bold text-info">Agent Group Preview</h5>
-                    <div class="container ">
-                        <ul class=" list-group text-left agents">
-                            <li class="  list-group-item list-group-item-info">
-                                <h5>
-                                    Fire Department
-                                </h5>
-                                <ul class="list-group">
-                                    <li class="list-group-item border-0">
-                                        <p>Tolgahan Dayanıklı</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="list-group-item list-group-item-info">
-                                <h5>
-                                    Police Department
-                                </h5>
-                                <ul class="list-group">
-                                    <li class="list-group-item border-0 ">
-                                        <p>Berkan Ergil</p>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="list-group-item list-group-item-info">
-                                <h5>
-                                    Health Department
-                                </h5>
-                                <ul class="list-group">
-                                    <li class="list-group-item border-0">
-                                        <p>Mehmet Taçyıldız</p>
-                                    </li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </div>
-                    <form class="mt-3" action="">
-                        <button type="button" id="deneme" class="btn btn-lg btn-info"><i class="fas fa-users"></i> Create Group</button>
-                    </form>
-                </div>
-            </div> --}}
         </div>
-
     </div>
+</form>
 
 @endsection
 
 
 @section("sweetjs")
 <script>
-    $(document).ready(function() {
-        $(".available_agents").select2()
-});
+        if ($('.available_agents').length > 0) {
+    $('.available_agents').select2();    
+  };
 
 </script>
 @endsection
-
